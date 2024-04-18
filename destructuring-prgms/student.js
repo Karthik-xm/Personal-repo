d0cbeb31b5a1af5b686e92e2340b42551a48fa97
Karthik <karthikbs@xminds.com>
2024-04-18 18:23:11 +0530

    destructure prgms

---
+++ destructuring-prgms/student.js
@@ -0,0 +1,18 @@
+const student = {
+    rollno: 123,
+    course: "Computer Science",
+    duration: "4 years",
+    startDate: "2022-09-01",
+    endDate: "2026-05-31",
+    grade: "A"
+  };
+  
+  const { rollno, course, duration, startDate, endDate, grade } = student;
+  
+  console.log("Roll No:", rollno);
+  console.log("Course:", course);
+  console.log("Duration:", duration);
+  console.log("Start Date:", startDate);
+  console.log("End Date:", endDate);
+  console.log("Grade:", grade);
+  
--- destructuring-prgms/swap-name.js
+++ destructuring-prgms/swap-name.js
@@ -9,5 +9,13 @@ console.log("names:", names);
 
 console.log("After swapping:");
 console.log("names:", names);
+
+const [first, second, third, fourth, fifth, sixth] = names;
+console.log("First:", first); 
+console.log("Second:", second);
+console.log("Third:", third); 
+console.log("Fourth:", fourth); 
+console.log("Fifth:", fifth);  
+console.log("Sixth:", sixth);
 }
 swap();
