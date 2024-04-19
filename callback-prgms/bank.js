d8beb26b7049e2753555ca39c58d308fa1628488
Karthik <karthikbs@xminds.com>
2024-04-19 16:57:12 +0530

    bank prgrm update

--- callback-prgms/bank.js
+++ callback-prgms/bank.js
@@ -1,26 +1,35 @@
-function BankAccount(acNo, balance) {
-    this.acNo = acNo;
-    this.balance = balance;
-}
-
-function checkLoanEligibility(acNo, balance, callback) {
-    var isEligible = balance >= 5000;
-
-    callback(isEligible);
-}
-
-function displayLoanEligibilityMessage(isEligible) {
-    if (isEligible) {
-        console.log("Eligible for a loan.");
-    } else {
-        console.log("Not eligible for a loan.");
+class BankAccount {
+    constructor(acNo, balance) {
+        this.acNo = acNo;
+        this.balance = balance;
     }
 }
 
-const prompt = require('prompt-sync')();
-var acNo = prompt("Enter account number:");
-var balance = parseFloat(prompt("Enter balance:"));
+function isLoanEligible(account, callback) {
+    return callback(account);
+}
 
-var customerAccount = new BankAccount(acNo, balance);
+function checkBalanceForLoan(account) {
+    return account.balance >= 5000;
+}
 
-checkLoanEligibility(customerAccount.acNo, customerAccount.balance, displayLoanEligibilityMessage);
+function promptForBankAccount() {
+    const prompt = require('prompt-sync')();
+    const acNo = prompt("Enter account number:");
+    const balance = parseFloat(prompt("Enter account balance:"));
+    return new BankAccount(acNo, balance);
+}
+
+function main() {
+    const account = promptForBankAccount();
+
+    const eligibleForLoan = isLoanEligible(account, checkBalanceForLoan);
+
+    if (eligibleForLoan) {
+        console.log("Eligible for loan.");
+    } else {
+        console.log("Not eligible for loan.");
+    }
+}
+
+main();
