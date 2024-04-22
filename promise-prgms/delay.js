837280e0efdcf2caeb112160b34fd6339d28e9bd
Karthik <karthikbs@xminds.com>
2024-04-22 16:57:27 +0530

    promise update

--- promise-prgms/delay.js
+++ promise-prgms/delay.js
@@ -1,13 +1,27 @@
+  
 const delayPromise = new Promise((resolve, reject) => {
     console.log("Program is in progress...");
     setTimeout(() => {
+      const data = { message: "Promise completed successfully." };
+      resolve(data);
+    }, 4000);
+    setTimeout(() => {
         reject("Promise rejected.");
-    }, 2000);
+    }, 5000);
   });
   
-  delayPromise.then(() => {
-    console.log("Promise completed successfully.");
+  const newPromise = delayPromise.then((data) => {
+    console.log(data);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve("Another promise resolved");
+        }, 2000);
+      });
+});
+
+newPromise.then((message) => {
+    console.log("Second promise:", message);
   }).catch((error) => {
-    console.log(error);
+    console.error("Error in second promise:", error);
   });
   
