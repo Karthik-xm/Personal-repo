e48bb6ab249744a2a4dca3864b55c314847a4f15
Karthik <karthikbs@xminds.com>
2024-04-24 15:48:32 +0530

    node module

--- async-prgms/names.js
+++ async-prgms/names.js
@@ -1,12 +1,29 @@
+// const displayNames = async () => {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users');
+
+//     const data = await response.json();
+//     const names = data.map(user => user.name);
+//     console.log("Names from api:");
+//     names.forEach(name => {
+//         console.log(`=> ${name}`);
+//     });
+// }
+
+// displayNames();
+
+
 const displayNames = async () => {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
-
     const data = await response.json();
-    const names = data.map(user => user.name);
-    console.log("Names from api:");
-    names.forEach(name => {
-        console.log(`=> ${name}`);
-    });
+    return data;
+    
 }
 
-displayNames();
+displayNames().then(data=>{
+     
+     console.log("Names from api:");
+     data.forEach(element => {
+        console.log(`${element.name}`);
+     });
+      
+      });
--- destructuring-prgms/swaping.js
+++ destructuring-prgms/swaping.js
@@ -1,18 +1,23 @@
-const swap =(a,b) => {
+//use of external module
+const calculator = require('../my-node-project/calculatorFunction.js');
+const swap = (a, b) => {
 
-console.log("Before swapping:");
-console.log("a:", a);
-console.log("b:", b);
+    console.log('sum of numbers: ', calculator)
+    console.log(calculator('+', a, b));
 
-[a, b] = [b, a];
+    console.log("Before swapping:");
+    console.log("a:", a);
+    console.log("b:", b);
 
-console.log("After swapping:");
-console.log("a:", a);
-console.log("b:", b);
+    [a, b] = [b, a];
+
+    console.log("After swapping:");
+    console.log("a:", a);
+    console.log("b:", b);
 }
 
 const prompt = require('prompt-sync')();
-a = prompt("Enter a number: ");
-b = prompt("Enter an another number: ");
-swap(a,b)
+a = parseInt(prompt("Enter a number: "));
+b = parseInt(prompt("Enter an another number: "));
+swap(a, b)
 
